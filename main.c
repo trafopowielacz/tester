755f2a7b0a267f300eaba731d2cb1e676f7ec4ab
trafopowielacz <dominikd1504@gmail.com>
2018-03-17 12:31:20 +0100

    miga se dioda

--- main.c
+++ main.c
@@ -5,39 +5,25 @@
  *      Author: dominik
  */
 
-//częstotliwość zegara
-#define F_CPU 1000000L
+
+#define F_CPU 8000000L
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-//definicja LED1 (do którego pinu podłączony LED1)
 #define LED1 PB0
 
-//definicja LED2 (do którego pinu podłączony LED2)
-#define LED2 PB1
 
 
-// Pętla główna
+
 void main(void)
 {
-    //########### I/O ###########
-    DDRB  |= (1<<LED1) | (1<<LED2); //Ustawienie pinów sterujących diodami
-                                         //jako wyjścia
-
-    PORTB |= (1<<LED1);    //Ustawienie stanu wysokiego na wyjściu
-                                 //sterującym LED1 (stan początkowy)
-     //##########################
-
-   while (1) //Pętla główna
+    DDRB  |= (1<<LED1);
+    PORTB |= (1<<LED1);
+   while (1)
    {
-
-        PORTB ^=(1<<LED1); //suma modulo 2 (XOR) stanu poprzedniego na porcie
-                             //pinu LED1 (zmiana stanu na przeciwny)
-
-        PORTB ^=(1<<LED2); //suma modulo 2 (XOR) stanu poprzedniego na porcie
-                             //pinu LED2 (zmiana stanu na przeciwny)
-
-        _delay_ms(1000); //opóźnienie 1000ms czyli 1s
+        PORTB ^=(1<<LED1);
+        _delay_ms(100);
+        _delay_ms(100);
     }
 }
