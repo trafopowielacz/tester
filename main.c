e5904478c6fc3b7095f7903960354b50c7988cf5
Mariusz <superhiro2a@gmail.com>
2018-03-17 16:39:37 +0100

    d

--- main.c
+++ main.c
@@ -9,12 +9,21 @@
 
 //#define F_CPU 8000000L  //nie pisz tego jak korzystasz z eclipse bo mogÄ… byc bÅ‚Ä™dy
 
-//chyba wszystko ok cos tu napisa³em widzisz?
-//abc
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include "LCD/lcd44780.h"
+
+// Makra upraszczaj¹ce dostêp do portów
+// *** PORT
+#define PORT(x) SPORT(x)
+#define SPORT(x) (PORT##x)
+// *** PIN
+#define PIN(x) SPIN(x)
+#define SPIN(x) (PIN##x)
+// *** DDR
+#define DDR(x) SDDR(x)
+#define SDDR(x) (DDR##x)
+
 
 #define LED1 PB0
 #define SW PB1
@@ -22,6 +31,8 @@
 
 void main(void)
 {
+	uint8_t key_lock=0;
+
     DDRB  |= (1<<LED1);
 
 
@@ -30,7 +41,7 @@ void main(void)
 
    while (1)
    {
-        if(  !(PINB & (1<<SW) )) PORTB ^= (1<<LED1) ;
+        if(!(PINB&(1<<SW) )) PORTB ^= (1<<LED1) ;
         else ;
         _delay_ms(100);
         _delay_ms(100);
