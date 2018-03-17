995f9bbb213cf1e07fce2b78b76abc049c3aa51c
Mariusz <superhiro2a@gmail.com>
2018-03-17 15:54:07 +0100

    a ok

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 
 //#define F_CPU 8000000L  //nie pisz tego jak kozystasz z eclipse bo mogÄ… byc bÅ‚Ä™dy
+//nie mogê pisac ci tego polskiego ci jak ¹cê¿Ÿó
 
 #include <avr/io.h>
 #include <util/delay.h>
